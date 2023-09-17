2a43d8a8e1df2cede3e454fe3e299289acbe01fc
Filip Cihak <cihakfil@fit.cvut.cz>
2023-09-17 16:26:48 +0200

    Spring initializr setup

---
+++ src/main/java/cz/cvut/fit/anteater/AnteaterApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.anteater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnteaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnteaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/anteater/AnteaterApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.anteater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnteaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
